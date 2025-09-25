44454f880106b08dc4a33753800cc6dbabfad0c0
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-09-25 21:41:04 +0000

    Update docker/devcontainers for hugo

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -euo pipefail
 
+sudo apt update && sudo apt install -y hugo
 
-pip3 install -r requirements.txt -r requirements_test.txt
-curl -L https://github.com/CloudCannon/pagefind/releases/download/v1.1.0/pagefind-v1.1.0-x86_64-unknown-linux-musl.tar.gz | tar -xz -C ~/.local/bin
+pip3 install -r requirements_test.txt
+pre-commit install
+
+mkdir -p ~/.local/bin
+curl -L https://github.com/CloudCannon/pagefind/releases/download/v1.3.0/pagefind-v1.3.0-x86_64-unknown-linux-musl.tar.gz | tar -xz -C ~/.local/bin
