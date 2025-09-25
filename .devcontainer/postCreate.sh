9a8b17b3bbf7979f45fa1d61eaf6000338ee24b0
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-09-25 22:08:46 +0000

    Use arch for pagefind

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,4 +7,15 @@ pip3 install -r requirements_test.txt
 pre-commit install
 
 mkdir -p ~/.local/bin
-curl -L https://github.com/CloudCannon/pagefind/releases/download/v1.3.0/pagefind-v1.3.0-x86_64-unknown-linux-musl.tar.gz | tar -xz -C ~/.local/bin
+
+ARCH="$(uname -m)"
+case "$ARCH" in
+  x86_64) PAGEFIND_ARCH="x86_64" ;;
+  arm64|aarch64) PAGEFIND_ARCH="aarch64" ;;
+  *)
+    echo "Unsupported architecture: $ARCH" >&2
+    exit 1
+    ;;
+esac
+curl -L "https://github.com/CloudCannon/pagefind/releases/download/v1.3.0/pagefind-v1.3.0-${PAGEFIND_ARCH}-unknown-linux-musl.tar.gz" \
+  | tar -xz -C ~/.local/bin
